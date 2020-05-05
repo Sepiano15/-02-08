2c0eb6b77b4bc51efdf9dad4462dc89c354d70b9
Noriaki77 <sepiano15@gmail.com>
2020-05-05 11:17:05 +0900

    Create Homework#8.py
    사각형과 원 그리기 구현

---
+++ Homework#8.py
@@ -0,0 +1,79 @@
+import turtle
+import random
+
+## 클래스 선언 부분 ##
+class Shape:  # 부모 클래스
+    myTurtle = None
+    cx, cy = 0, 0  # 사각형 및 원의 중심점.
+
+    def __init__(self):
+        self.myTurtle = turtle.Turtle('turtle') # 거북이 생성.
+        self.myTurtle.hideturtle()
+
+    def setPen(self) : # 펜 색상과 두께를 랜덤하게 뽑기
+        r = random.random()
+        g = random.random()
+        b = random.random()
+        self.myTurtle.pencolor((r, g, b))
+        pSize = random.randrange(1,10)
+        self.myTurtle.pensize(pSize)
+
+    def drawShape(self):  # 하위 클래스에서 상속받아서 오버라이딩
+        pass
+
+class Rectangle(Shape): # 자식 클래스
+    width, height = [0] * 2
+    def __init__(self, x, y):
+        Shape.__init__(self)
+        self.cx = x #실제 좌표
+        self.cy = y
+        self.width = random.randrange(20, 100)
+        self.height = random.randrange(20, 100)
+
+    def drawShape(self) :
+        # 네모 그리기
+        sx1, sy1, sx2, sy2 = [0] * 4  # 왼쪽 위 X, Y와 오른쪽 아래 X,Y
+
+        sx1 = self.cx - self.width/2 #왼쪽 위 모서리의 x좌표
+        sy1 = self.cy - self.height /2 #왼쪽 위 모서리의 y좌표
+        sx2 = self.cx + self.width/2
+        sy2 = self.cy + self.height /2
+
+        self.setPen() # 부모 클래스 메서드
+        self.myTurtle.penup()
+        self.myTurtle.goto(sx1, sy1) #왼쪽 위로 가시오
+        self.myTurtle.pendown()
+        self.myTurtle.goto(sx1, sy2) #오른쪽 위로 드로잉
+        self.myTurtle.goto(sx2, sy2) #오른쪽 밑
+        self.myTurtle.goto(sx2, sy1) #왼쪽 밑
+        self.myTurtle.goto(sx1, sy1) #처음
+
+class Circle(Shape):
+    def __init__(self, x, y):
+        Shape.__init__(self)
+        self.cx = x #실제 좌표
+        self.cy = y
+
+    def drawShape(self):
+        length = random.randrange(20, 100)
+        self.setPen()
+        self.myTurtle.penup()
+        self.myTurtle.goto(self.cx, self.cy) #왼쪽 위로 가시오
+        self.myTurtle.pendown()
+        self.myTurtle.speed(13)
+        self.myTurtle.circle(length)
+
+## 함수 선언 부분 ##
+def  screenLeftClick(x,y):
+    rect = Rectangle(x, y)
+    rect.drawShape()
+
+def screenRightClick(x,y):
+    circle = Circle(x,y)
+    circle.drawShape()
+
+## 메인 코드 부분 ##
+turtle.title('거북이로 객체지향 사각형  그리기')
+turtle.onscreenclick(screenLeftClick,1) #onscreenclick안의 함수에 현재 클릭한 x,y좌표를 준다.
+turtle.onscreenclick(screenRightClick,3)
+turtle.done()
